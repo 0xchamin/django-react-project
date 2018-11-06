9474cf2cd11d742a3d95a720022b950f9b0c0c25
chaminn <chmk90@gmail.com>
2018-11-06 17:57:25 +0800

    completed crud for articles

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -128,8 +128,9 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    'rest_framework.permissions.AllowAny',
+        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
 
-CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ORIGIN_ALLOW_ALL = True
